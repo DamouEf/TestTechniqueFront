058152b0719a7cf51ba952771d79941996a5ca68
Wael FRIDHI <fridhiwael@gmail.com>
2022-09-04 18:44:25 +0100

    create services

---
+++ src/app/services/control.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Control } from '@app/shared/models/control.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ControlService {
+
+  public controlUrl = '/api/v1/controls/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getControl(): Observable<Control[]> {
+    const headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json; charset = utf-8', 'Pagination-Style': 'PageNumberPagination' });
+    return this.httpClient.request<Control[]>('GET', this.controlUrl, {headers});
+  }
+}
